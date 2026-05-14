32b6d1d3c4425e14389e7909cfcbeb75acf34824
Ronak Tailor <tailorronak6@gmail.com>
2026-05-14 10:40:48 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+# Q => Create Account class with 2 attrubutes - balance & account no. Create methods for dibit, credit & printing the balance.
+
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
+
+    # debit method
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs.", amount, "was debited")
+        print("Total balance =", self.get_balance())
+
+    # credit method
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs.", amount, "was credited")
+        print("Total balance =", self.get_balance())
+
+    # balance check
+    def get_balance(self):
+        return self.balance
+
+
+acc1 = Account(100000, 1234567)
+print(acc1.balance)
+print(acc1.account_no)
+acc1.debit(1000)
+acc1.credit(500)
